f956bb0cf1f5a453014cd493d92d27f80d3debd5
giftlebo166@gmail.com <giftlebo166@gmail.com>
2026-07-26 14:43:24 +0000

    code

--- Temporary Table | CTEs.sql
+++ Temporary Table | CTEs.sql
@@ -10,3 +10,19 @@ WHERE age >40
 
 SELECT *
 FROM elders;
+
+WITH cte1 AS (
+    SELECT name,
+           email,
+           age 
+    FROM june_intake.student_profiles.student_data
+    WHERE age > 40
+)
+SELECT *
+FROM cte1;
+
+
+
+
+
+
